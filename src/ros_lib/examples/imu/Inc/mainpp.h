0a592d11ac9879e380caf4c3438b59c737ed0e18
Kenta Yonekura <yoneken@ieee.org>
2018-03-02 00:26:51 +0900

    [Readme] Update links to English websites. fix #1

--- src/ros_lib/examples/imu/Inc/mainpp.h
+++ src/ros_lib/examples/imu/Inc/mainpp.h
@@ -15,6 +15,10 @@
 void setup(void);
 void loop(void);
 
+void SPI_Mem_Write(unsigned short int chip, uint8_t address, short num, const uint8_t data[]);
+void SPI_Mem_Read(unsigned short int chip, uint8_t address, void (*callback)(uint8_t dat[]), short num);
+
+
 #ifdef __cplusplus
 }
 #endif
--- src/ros_lib/examples/imu/Src/mainpp.cpp
+++ src/ros_lib/examples/imu/Src/mainpp.cpp
@@ -8,6 +8,8 @@
 #include "stm32f3xx_hal.h"
 #include "stm32f3xx_hal_spi.h"
 #include "mpu9250.h"
+#include "inv_mpu.h"
+#include "inv_mpu_dmp_motion_driver.h"
 #include <mainpp.h>
 #include <queue>
 #include <memory>
@@ -70,12 +72,13 @@ void SPI_Mem_Transmit(void){
   }
 }
 
-void SPI_Mem_Write(unsigned short int chip, uint8_t address, short num, const uint8_t data[]){
+uint8_t SPI_Mem_Write(unsigned short int chip, uint8_t address, short num, const uint8_t data[]){
   spiq.push(std::unique_ptr<SPI_Mem>(new SPI_Mem(chip, address, num, data)));
   SPI_Mem_Transmit();
+  return 1;
 }
 
-void SPI_Mem_Read(unsigned short int chip, uint8_t address, void (*callback)(uint8_t dat[]), short num){
+void SPI_Mem_Read(unsigned short int chip, uint8_t address,  short num, void (*callback)(uint8_t dat[])){
   spiq.push(std::unique_ptr<SPI_Mem>(new SPI_Mem(chip, address | 0x80, callback, num)));
   SPI_Mem_Transmit();
 }
@@ -88,28 +91,6 @@ void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi){
   spiq.pop();
   flag_transmit = false;
   if(!spiq.empty()) SPI_Mem_Transmit();
-}
-
-void cb_whoami(uint8_t dat[]){
-  uint8_t tmp = dat[0];	// expect MPU9250-0x71(113), MPU9255-0x73(115)
-}
-
-void cb_sense(uint8_t dat[]){
-  short acc[3], temp, gyro[3];
-  acc[0] = dat[0] << 8 | dat[1];
-  acc[1] = dat[2] << 8 | dat[3];
-  acc[2] = dat[4] << 8 | dat[5];
-  temp = dat[6] << 8 | dat[7];
-  gyro[0] = dat[8] << 8 | dat[9];
-  gyro[1] = dat[10] << 8 | dat[11];
-  gyro[2] = dat[12] << 8 | dat[13];
-}
-
-void cb_mag_sense(uint8_t dat[]){
-  short magnet[3];
-  magnet[0] = dat[0] | dat[1] << 8;
-  magnet[1] = dat[2] | dat[3] << 8;
-  magnet[2] = dat[4] | dat[5] << 8;
 }
 
 void setup(void)
@@ -156,13 +137,100 @@ void setup(void)
   SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_DO, 1, (uint8_t*)cntl1);	// expect 0x16(22)
 
   SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_CTRL, 1, (uint8_t*)i2cctr);	// expect 0x81(129)
+/*
+  // porting from motion_driver
+  inv_error_t result;
+  result = mpu_init(&int_param);
+  result = inv_init_mpl();
+
+  inv_enable_quaternion();
+  inv_enable_9x_sensor_fusion();
+
+  inv_enable_fast_nomot();
+  // inv_enable_motion_no_motion();
+  // inv_set_no_motion_time(1000);
+
+  inv_enable_gyro_tc();
+
+  // inv_enable_in_use_auto_calibration();
+
+  inv_enable_vector_compass_cal();
+  inv_enable_magnetic_disturbance();
+
+  result = inv_start_mpl();
+
+  mpu_set_sensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
+  mpu_configure_fifo(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
+
+  mpu_set_sample_rate(DEFAULT_MPU_HZ);
+  mpu_set_compass_sample_rate(1000 / COMPASS_READ_MS);
+  mpu_get_sample_rate(&gyro_rate);
+  mpu_get_gyro_fsr(&gyro_fsr);
+  mpu_get_accel_fsr(&accel_fsr);
+  mpu_get_compass_fsr(&compass_fsr);
+
+  inv_set_gyro_sample_rate(1000000L / gyro_rate);
+  inv_set_accel_sample_rate(1000000L / gyro_rate);
+  inv_set_compass_sample_rate(COMPASS_READ_MS * 1000L);
+
+  inv_set_gyro_orientation_and_scale(
+          inv_orientation_matrix_to_scalar(gyro_pdata.orientation),
+          (long)gyro_fsr<<15);
+  inv_set_accel_orientation_and_scale(
+          inv_orientation_matrix_to_scalar(gyro_pdata.orientation),
+          (long)accel_fsr<<15);
+  inv_set_compass_orientation_and_scale(
+          inv_orientation_matrix_to_scalar(compass_pdata.orientation),
+          (long)compass_fsr<<15);
+
+  dmp_load_motion_driver_firmware();
+  dmp_set_orientation(
+      inv_orientation_matrix_to_scalar(gyro_pdata.orientation));
+
+  dmp_enable_feature(DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_SEND_RAW_ACCEL | DMP_FEATURE_SEND_CAL_GYRO | DMP_FEATURE_GYRO_CAL);
+  dmp_set_fifo_rate(DEFAULT_MPU_HZ);
+  mpu_set_dmp_state(1);
+*/
+}
+/*
+void build_gyroacc(short *gyro, short *accel_short, unsigned long *timestamp, unsigned char *sensors)
+{
+  long accel[3], temperature;
+
+  if(sensors & INV_XYZ_GYRO){
+    inv_build_gyro(gyro, timestamp);
+    if(new_temp) {
+      new_temp = 0;
+      mpu_get_temperature(&temperature, &timestamp);
+      inv_build_temp(temperature, timestamp);
+    }
+  }
+
+  if(sensors & INV_XYZ_ACCEL){
+    accel[0] = (long)accel_short[0];
+    accel[1] = (long)accel_short[1];
+    accel[2] = (long)accel_short[2];
+    inv_build_accel(accel, 0, timestamp);
+  }
 
 }
-
+*/
 void loop(void)
 {
-  //SPI_Mem_Read(CS_IMU_Pin, WHO_AM_I, cb_whoami, 1);
-  SPI_Mem_Read(CS_IMU_Pin, ACCEL_XOUT_H, cb_sense, 14);
+  SPI_Mem_Read(CS_IMU_Pin, WHO_AM_I, 1, [](uint8_t dat[]){
+    uint8_t tmp = dat[0];	// expect MPU9250-0x71(113), MPU9255-0x73(115)
+  });
+
+  SPI_Mem_Read(CS_IMU_Pin, ACCEL_XOUT_H, 14, [](uint8_t dat[]){
+    short acc[3], temp, gyro[3];
+    acc[0] = dat[0] << 8 | dat[1];
+    acc[1] = dat[2] << 8 | dat[3];
+    acc[2] = dat[4] << 8 | dat[5];
+    temp = dat[6] << 8 | dat[7];
+    gyro[0] = dat[8] << 8 | dat[9];
+    gyro[1] = dat[10] << 8 | dat[11];
+    gyro[2] = dat[12] << 8 | dat[13];
+  });
 
   // Read magnetometer through I2C
   I2C_SLV0_ADDR_REG i2caddr[1] = {0};
@@ -179,7 +247,12 @@ void loop(void)
   i2cctr[0].I2C_SLV0_LENG = 7;	// Read 7 byte from HXL to ST2
   SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_CTRL, 1, (uint8_t*)i2cctr);	// expect 0x87(135)
 
-  SPI_Mem_Read(CS_IMU_Pin, EXT_SENS_DATA, cb_mag_sense, 7);
+  SPI_Mem_Read(CS_IMU_Pin, EXT_SENS_DATA, 7, [](uint8_t dat[]){
+    short magnet[3];
+    magnet[0] = dat[0] | dat[1] << 8;
+    magnet[1] = dat[2] | dat[3] << 8;
+    magnet[2] = dat[4] | dat[5] << 8;
+  });
   /*
   if(!flag){
     flag = true;
@@ -201,6 +274,49 @@ void loop(void)
     HAL_GPIO_WritePin(GPIOA, CS_IMU_Pin, GPIO_PIN_SET);
   }
   */
+
+  // porting
+/*
+  short gyro[3], accel_short[3], sensors;
+  unsigned char more;
+  long accel[3], quat[4], temperature;
+
+  dmp_read_fifo(gyro, accel_short, quat, &sensor_timestamp, &sensors, &more);
+  build_gyroacc(gyro, accel_short, &sensor_timestamp, &sensors);
+
+  if(sensors & INV_WXYZ_QUAT){
+    inv_build_quat(quat, 0, sensor_timestamp);
+  }
+
+  if(more){
+    mpu_read_fifo(gyro, accel_short, &sensor_timestamp, &sensors, &more);
+    build_gyroacc(gyro, accel_short, &sensor_timestamp, &sensors);
+  }
+
+  if(newdata){
+    inv_execute_on_data();
+
+    float float_data[3] = {0};
+    long data[9];
+    int8_t accuracy;
+    unsigned long timestamp;
+    if(inv_get_sensor_type_quat(data, &accuracy, (inv_time_t*)&timestamp)){
+      // quaternion
+    }
+    if(inv_get_sensor_type_accel(data, &accuracy, (inv_time_t*)&timestamp)){
+      // acc
+    }
+    if(inv_get_sensor_type_gyro(data, &accuracy, (inv_time_t*)&timestamp)){
+      // gyro
+    }
+    if(inv_get_sensor_type_compass(data, &accuracy, (inv_time_t*)&timestamp)){
+      // compass
+    }
+    if(inv_get_sensor_type_linear_acceleration(float_data, &accuracy, (inv_time_t*)&timestamp)){
+      // linear acc
+    }
+  }
+*/
   HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
 
   nh.spinOnce();
